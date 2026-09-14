0029c6c84a250825f50fc44e594e4d838b600562
Abhijeet Gawali <gawalishambu@gmail.com>
2026-09-14 20:04:17 +0530

    Add mongoose dependency and create User model with Message schema

---
+++ src/model/User.ts
@@ -0,0 +1,29 @@
+import mongoose, {Schema,Document} from "mongoose";
+
+export interface Message extends Document{
+    content: string;
+    createdAt: Date;
+}
+
+const MessageSchema: Schema<Message> = new Schema({
+    content: {
+        type: String,
+        requried: true
+    },
+
+    createdAt: {
+        type: Date,
+        required: true,
+        default: Date.now
+    }
+})
+
+
+export interface User extends Document{
+    username: string;
+    email: string;
+    password: string;
+    verifyCode: string;
+    verifycodeExpiry:Date;
+    message: Message[]
+}
