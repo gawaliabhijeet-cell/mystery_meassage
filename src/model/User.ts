73877c001aa7568f9335c0d9d34fc13ad30e2de7
Abhijeet Gawali <gawalishambu@gmail.com>
2026-09-14 20:23:12 +0530

    Refactor User model to include message handling and ensure proper schema definition

--- src/model/User.ts
+++ src/model/User.ts
@@ -25,5 +25,56 @@ export interface User extends Document{
     password: string;
     verifyCode: string;
     verifycodeExpiry:Date;
-    message: Message[]
+    isVerified: boolean;
+    isAcceptingMessage: boolean;
+    messages: Message[]
 }
+
+const UserSchema: Schema<User> = new Schema({
+    username: {
+        type: String,
+        required: [true, "Username is required"],
+        trim: true,
+        unique: true
+    },
+
+    email: {
+        type: String,
+         required: [true, "email is required"],
+         unique: true,
+         match: [/.+@+\..+/, 'please use valid email address']
+    },
+
+    password: {
+        type: String,
+          required: [true, "password is required"],
+
+    },
+    verifyCode: {
+        type: String,
+          required: [true, "Verify code is required"],
+
+    },
+    verifycodeExpiry: {
+        type: Date,
+          required: [true, "VerifycodeExpiry is required"],
+
+    },
+    isVerified: {
+        type: Boolean,
+        default: false,
+    },
+    isAcceptingMessage: {
+        type: Boolean,
+        default: true,
+    },
+    messages: [MessageSchema]
+
+
+})
+
+
+const UserModel = (mongoose.models.User as mongoose.Model<User>) ||
+    mongoose.model<User>("User", UserSchema)
+
+ export default UserModel;
