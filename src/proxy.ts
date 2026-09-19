ccf0c97ac66536b822086a8192ca70f97ad3f326
Abhijeet Gawali <gawalishambu@gmail.com>
2026-09-19 11:45:39 +0530

    Add  middleware to proxy   for authentication redirection

--- src/proxy.ts
+++ src/proxy.ts
@@ -5,7 +5,7 @@ export const config = {
   matcher: ['/dashboard/:path*', '/sign-in', '/sign-up', '/', '/verify/:path*'],
 };
 
-export async function middleware(request: NextRequest) {
+export async function proxy(request: NextRequest) {
   const token = await getToken({ req: request });
   const url = request.nextUrl;
 
